29665e609de31545b08a4c35d446ecd4a8aadd12
Mr-Murari <94611039+Mr-Murari@users.noreply.github.com>
2023-05-08 11:21:27 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,15 +5,15 @@ from tkinter.filedialog import askopenfile
 
 #reading test image
 #file=askopenfile(filetypes=[("Image file","*.png")])
-img = cv2.imread("C:\\Users\\MURARI\\Desktop\\POTHOLE\\pothole-detection\\p2.jfif") #image name
+img = cv2.imread("pothole-detection\\p2.jfif") #image name
 #img = cv2.imread(file) #image name
 
 #reading label name from obj.names file
-with open(os.path.join("C:\\Users\\MURARI\\Desktop\\POTHOLE\\pothole-detection\\project_files",'obj.names'), 'r') as f:
+with open(os.path.join("pothole-detection\\project_files",'obj.names'), 'r') as f:
     classes = f.read().splitlines()
 
 #importing model weights and config file
-net = cv2.dnn.readNet("C:\\Users\\MURARI\\Desktop\\POTHOLE\\pothole-detection\\project_files\\yolov4_tiny.weights","C:\\Users\\MURARI\\Desktop\\POTHOLE\\pothole-detection\\project_files\\yolov4_tiny.cfg")
+net = cv2.dnn.readNet("pothole-detection\\project_files\\yolov4_tiny.weights","pothole-detection\\project_files\\yolov4_tiny.cfg")
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
 classIds, scores, boxes = model.detect(img, confThreshold=0.6, nmsThreshold=0.4)
